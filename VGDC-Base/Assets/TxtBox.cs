261610c589372fea4d0b7083a072f9849db9298d
MacdonEm <macdonaldel17@gmail.com>
2018-05-01 13:35:49 -0400

    Potentially text box
    Potentially

---
+++ VGDC-Base/Assets/TxtBox.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TxtBox : MonoBehaviour
+{
+
+    public bool loop;
+    public bool lookAt;
+    public string[] conversation;
+    public float damping = 3.5f;
+
+    private int convoIndex = 0;
+    private string currentTalk = "";
+    private bool playerIn = false;
+    // Use this for initialization
+    private GUIText pGUI;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+
+        if (lookAt)
+        {
+            smoothLookAtIgnoreHeight(col.transform);
+        }
+
+        bool pressed = false;
+
+        if (col.gameObject.tag == "Player")
+        {
+
+            currentTalk = conversation[convoIndex];
+
+            if (Input.GetKeyDown("z"))
+            {
+                pressed = true;
+            }
+
+            if (Input.GetKeyUp("z"))
+            {
+                if (pressed == true)
+                {
+                    convoIndex++;
+                    pressed = false;
+                    if (convoIndex >= conversation.Length)
+                    {
+                        if (loop) convoIndex = 0;
+                        else convoIndex = conversation.Length - 1;
+                    }
+                }
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            currentTalk = "";
+            playerIn = false;
+        }
+    }
+
+    void OnGUI()
+    {
+
+        GUI.Label(new Rect(0, 0, Screen.width, 20), currentTalk);
+    }
+
+    void LookAtIgnoreHeight(Transform target)
+    {
+
+        Vector3 lookAtPos = target.position;
+        //Set y of LookAt target to be my height.
+        lookAtPos.y = transform.position.y;
+        transform.LookAt(lookAtPos);
+    }
+
+    void smoothLookAtIgnoreHeight(Transform target)
+    {
+        Vector3 targetPos = target.position;
+
+        targetPos.y = transform.position.y;
+
+        Quaternion rotation = Quaternion.LookRotation(targetPos - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+    }
+
+}
